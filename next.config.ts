15e960870ea6e0d5ee9301a8c693c1c61cfe8623
Himanshu Raj <himanshuraj2018nna@gmail.com>
2026-06-05 18:34:28 +0530

    third commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Ye Next.js ko static HTML generate karne ko bolega
+  images: {
+    unoptimized: true, // GitHub Pages par images chalane ke liye zaroori hai
+  },
+  // Agar aapka repository name 'healthcare-demo' hai, toh ye dono lines add karein:
+  basePath: '/healthcare-demo',
+  assetPrefix: '/healthcare-demo',
 };
 
 export default nextConfig;
