60d441dce51f07899bf15a2a7e76d7b5ef79545b
Himanshu Raj <himanshuraj2018nna@gmail.com>
2026-06-05 18:40:39 +0530

    fouth commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Ye Next.js ko static HTML generate karne ko bolega
-  images: {
-    unoptimized: true, // GitHub Pages par images chalane ke liye zaroori hai
-  },
-  // Agar aapka repository name 'healthcare-demo' hai, toh ye dono lines add karein:
-  basePath: '/healthcare-demo',
-  assetPrefix: '/healthcare-demo',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* Vercel par bina kisi extra paths ke direct chalta hai */
 };
 
 export default nextConfig;
